6499198060ea3f4bd93d9d85bf3e8b90390b5cb1
ltkcl <167499996+ltkcl@users.noreply.github.com>
2025-12-28 22:24:29 +0530

    Delete backend directory

--- backend/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-const asyncHandler = (asyncHandler)=>{
-    return (req,res,next)=>{
-        Promise.resolve(asyncHandler(req,res,next)).catch((err)=>{
-            next(err);
-        })
-    }
-}
-export default asyncHandler;
